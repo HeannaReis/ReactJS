4393ef9c71244cf08b5b63fcf5fc418ebcd5c229
HennaReis <67883256+HeannaReis@users.noreply.github.com>
2022-04-30 01:29:49 -0300

    configurando webpack

--- Intro-ReactJS/projeto-dio/src/index.js
+++ Intro-ReactJS/projeto-dio/src/index.js
@@ -1,27 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./style.css"
-
-function sum(a,b){
-  return a + b
-} 
-
-function PrimeiroJSX (){
-  return (
-      <div className="teste">
-        Joel Heanna - Introdução ao ReactJS
-      <h2>Soma: {sum (10, 20)}</h2>
-      </div>  
-  )
-}
-
-const App = () => {
-    return (
-        <div className = "App">
-        {PrimeiroJSX()}
-        </div>
-    )
-} 
-
-const rootElement = document.getElementById("root")
-ReactDOM.render(<App />, rootElement )
+import React  from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("app"));
